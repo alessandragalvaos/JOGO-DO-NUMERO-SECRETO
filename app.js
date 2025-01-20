ff7f96b1122631ba19b4e0b02b376daf8a1c5951
alessandragalvaos <gsb.alessandra@gmail.com>
2025-01-20 11:07:26 -0300

    alterando o limite e descrevendo as funções

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 // Inicializando algumas variáveis
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
